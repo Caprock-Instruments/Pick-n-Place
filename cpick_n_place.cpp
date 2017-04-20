afde3db33bcf9d02d99ce4682407a8e393f8f478
Mathew-G-Pelletier <mathew.pelletier@ars.usda.gov>
2017-04-20 06:12:34 -0500

    added pick-n-place class

---
+++ cpick_n_place.cpp
@@ -0,0 +1,6 @@
+#include "cpick_n_place.h"
+
+cPick_n_Place::cPick_n_Place(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ cpick_n_place.h
@@ -0,0 +1,17 @@
+#ifndef CPICK_N_PLACE_H
+#define CPICK_N_PLACE_H
+
+#include <QObject>
+
+class cPick_n_Place : public QObject
+{
+    Q_OBJECT
+public:
+    explicit cPick_n_Place(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // CPICK_N_PLACE_H
